fb7aa985837fda54609dd0d8b9767ece4d150880
yaghoub <y.ghaderi@outlook.com>
2024-04-18 11:44:13 +0330

    add AdalineGD algorithm

--- classification.py
+++ classification.py
@@ -65,9 +65,76 @@ class Perceptron:
 
     def predict(self, X: np.array):
         """Return class label after unit step"""
-        return np.where(self.net_input(X) >= 0.0, 1, 0) 
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+
+
+class AdalineGD:
+    """
+    Adaptive Linear Neuron classifier.
+
+    Parameters
+    ----------
+    eta: float
+        Learning rate (between 0.0 and 1.0)
+    n_iter: int
+        Passes over the training dataset.
+    random_state: int
+        Random number generator seed for random weight initialization.
     
+    Attributes
+    ----------
+    w_ : 1d_array
+        weight after fitting.
+    b_ : Scalar
+        Bias unit after fitting.
+    losses_ : list
+        Mean squared error loss function values in each epoch.
+    """
 
+    def __init__(self, eta: int = 0.01, n_iter: int = 50, random_state: int = 1) -> None:
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
 
+    def fit(self, X, y):
+        """
+        Fit training data.
 
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+            Training vectors, where n_examples
+            is the number of examples and
+            n_features is the number of features.
+        y : {array-like}, shape = [n_examples]
+            Target values.
 
+        Returns
+        -------
+        self : object
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+        self.b_ = np.float_(0.)
+        self.losses_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_ = self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+            self.b_ = self.eta * 2.0 * errors.mean()
+            loss = (errors ** 2).mean()
+            self.losses_.append(loss)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return X
+
+    def predict(self, X):
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
